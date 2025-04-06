8c71af987cc35f281212bace90f8f72ca143ae9a
Michael Wallner <m.wallner@neonblack.at>
2025-04-06 14:32:03 +0200

    🎉 May the Force be with you, always

---
+++ components/ui/dropdown-menu/DropdownMenuSubTrigger.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { cn } from '@/lib/utils'
+import { DropdownMenuSubTrigger, type DropdownMenuSubTriggerProps, useForwardProps, } from 'reka-ui'
+import { computed, type HTMLAttributes } from 'vue'
+
+const props = defineProps<DropdownMenuSubTriggerProps & { class?: HTMLAttributes['class'] }>()
+
+const delegatedProps = computed(() => {
+  const { class: _, ...delegated } = props
+
+  return delegated
+})
+
+const forwardedProps = useForwardProps(delegatedProps)
+</script>
+
+<template>
+  <DropdownMenuSubTrigger
+    v-bind="forwardedProps"
+    :class="cn(
+      'flex cursor-default font-semibold select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none focus:bg-accent focus:text-accent-foreground data-[state=open]:bg-accent data-[state=open]:text-accent-foreground',
+      props.class,
+    )"
+  >
+    <slot/>
+    <Icon
+      name="lucide:chevron-right"
+      class="ml-auto"
+    />
+  </DropdownMenuSubTrigger>
+</template>

723a47fb2918d913af06c3eeefd8e0384a57a57e
Michael Wallner <m.wallner@coderscantina.com>
2025-09-08 15:29:27 +0200

    🩹 fix value update cycles

--- components/blocks/SchemaEditor.vue
+++ components/blocks/SchemaEditor.vue
@@ -276,7 +276,7 @@ const updateSchemaItem = (key: string, value: unknown) => {
 }
 
 watch(() => props.schema, (newSchema) => {
-  localSchema.value = { ...newSchema }
+  localSchema.value = JSON.parse(JSON.stringify(newSchema))
 }, { deep: true })
 
 watch(() => props.editor, (newEditor) => {
